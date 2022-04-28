0a5b7afb0dc273aeaa61be8c5f7b947a2c002df4
Aaron Boxer <boxerab@protonmail.com>
2022-04-27 23:43:12 -0400

    calculate per-strip number of write blocks

--- src/SeamCache.cpp
+++ src/SeamCache.cpp
@@ -21,6 +21,7 @@ SeamCache::~SeamCache() {
 		delete seamBuffers_[i];
 	delete[] seamBuffers_;
 }
+//note: there are (numStrips_-1) seam buffers : the final strip has none
 uint8_t* SeamCache::getSeamBuffer(uint32_t strip){
 	return (strip < numStrips_-1) ? seamBuffers_[strip]->data : nullptr;
 }
@@ -28,6 +29,7 @@ SeamInfo SeamCache::getSeamInfo(uint32_t strip){
 	SeamInfo ret;
 	ret.writeSize_ = init_.writeSize_;
 	ret.upperBegin_ = upperBegin(strip);
+	assert(ret.upperBegin_% init_.writeSize_ == 0);
 	ret.upperEnd_   = stripEnd(strip);
 	ret.lowerBegin_ = stripOffset(strip);
 	// no lower seam
@@ -36,9 +38,10 @@ SeamInfo SeamCache::getSeamInfo(uint32_t strip){
 		ret.lowerEnd_ = ret.lowerBegin_;
 	else
 		ret.lowerEnd_ = upperBegin(strip-1) + init_.writeSize_;
-
 	assert(ret.lowerEnd_% init_.writeSize_ == 0);
-	assert(ret.upperBegin_% init_.writeSize_ == 0);
+	assert((ret.upperBegin_ - ret.lowerEnd_) % init_.writeSize_ == 0);
+	ret.numWriteBlocks_ = (ret.upperBegin_ - ret.lowerEnd_) / init_.writeSize_;
+
 	return ret;
 }
 uint32_t SeamCache::getNumStrips(void){
