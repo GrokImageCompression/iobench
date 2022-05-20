c109daeaa314caa2261c5c709e1daaae76a23157
Aaron Boxer <boxerab@protonmail.com>
2022-05-20 17:38:20 -0400

    close should be marked override

--- src/iobench/FileIOUnix.h
+++ src/iobench/FileIOUnix.h
@@ -16,7 +16,7 @@ public:
 	void registerReclaimCallback(io_callback reclaim_callback, void* user_data) override;
 	bool attach(FileIOUnix* parent);
 	bool open(std::string name, std::string mode, bool asynch);
-	bool close(void);
+	bool close(void) override;
 	uint64_t write(uint64_t offset, IOBuf **buffers, uint32_t numBuffers) override;
 	uint64_t write(uint8_t* buf, uint64_t size);
 	uint64_t seek(int64_t off, int32_t whence);
